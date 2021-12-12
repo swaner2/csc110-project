a250fa2076c6a2efb0a0e5ba13a12b75c8616dbe
Jessica <86748529+swaner2@users.noreply.github.com>
2021-12-12 17:16:12 -0500

    Add files via upload

--- hate_crime.py
+++ hate_crime.py
@@ -1,4 +1,5 @@
 """CSC110 Project: The Hidden Correlation Between Covid-19 and Hate Crimes in the United States
+
 Module Description
 ==================
 This module contains the data and functions to read hate_crime.csv file.
@@ -12,19 +13,23 @@ from typing import List, Tuple
 @dataclass
 class HateCrime:
     """"A data type representing a specific hate crime instance
+
     This corresponds to one row of tabular data found in hate_crime.csv
+
     Attributes:
         - incident_id: incident_id of the hate crime incident
         - state: abbreviated state name
         - date: date of the hate crime incident
+
     Representation invariants:
         - incident_id >= 0
         - 1999 <= date.year
         - len(state_abbr) == 2
         - state in {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL',\
                     'IN', 'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT',\
-                    'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OK', 'OR', 'PA', 'RI', 'SC',\
-                    'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
+                    'NB', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',\
+                    'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
+
     Even though there are many more headers, it is not needed in our project.
     """
     incident_id: int
@@ -34,9 +39,12 @@ class HateCrime:
 
 def read_csv_file(filename: str) -> Tuple[List[str], List[HateCrime]]:
     """Return the headers and data stored in a csv file with the given filename.
+
         The return value is a tuple consisting of two elements:
+
         - The first is a list of strings for the headers of the csv file.
         - The second is a list of Delays (using the data class you just defined).
+
         Preconditions:
           - filename refers to a valid csv file with headers
     """
@@ -51,6 +59,7 @@ def read_csv_file(filename: str) -> Tuple[List[str], List[HateCrime]]:
 
 def process_row(row: List[str]) -> HateCrime:
     """Convert a row of hate crime incident data to HateCrime object.
+
     Preconditions:
         - row has the correct format for the hate crime data set.
     """
@@ -58,13 +67,15 @@ def process_row(row: List[str]) -> HateCrime:
         int(row[0]),
         row[6],
         str_to_date(row[12])
-    )
+        )
 
 
 def str_to_date(date_string: str) -> datetime.date:
     """Convert a string in yyyy-mm-dd format to a datetime.date
+
     Preconditions:
         - date_string has format yyyy-mm-dd
+
     >>> str_to_date('2011-01-05')
     datetime.date(2011, 1, 5)
     """
@@ -79,11 +90,12 @@ def str_to_date(date_string: str) -> datetime.date:
 ###############################################################################
 def num_instances_by_month(data: List[HateCrime], state: str, year: int, month: int) -> int:
     """Return the number of hate crime instances that occurred in the given state, month and year.
+
     Preconditions:
         - state in {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL',\
                     'IN', 'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT',\
-                    'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OK', 'OR', 'PA', 'RI', 'SC',\
-                    'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
+                    'NB', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',\
+                    'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
         - 1999 <= year <= 2020
         - 1 <= month <= 12
     """
@@ -98,12 +110,13 @@ def num_instances_by_month(data: List[HateCrime], state: str, year: int, month: 
 
 def num_instances_by_year(data: List[HateCrime], state: str, year: int) -> int:
     """Return the number of hate crime instances that occurred in the given state and year.
-        Preconditions:
-            - state in {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL',\
-                        'IN', 'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT',\
-                        'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OK', 'OR', 'PA', 'RI', 'SC',\
-                        'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
-            - 1999 <= year <= 2020
+
+    Preconditions:
+        - state in {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL',\
+                    'IN', 'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT',\
+                    'NB', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',\
+                    'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
+        - 1999 <= year <= 2020
     """
     num_of_instances = 0
 
@@ -118,9 +131,9 @@ def predictions(data: List[HateCrime]) -> dict[str, int]:
     """Takes hate crime data and returns a dictionary with the predicted number of hate crime
     incidents in 2020 per state"""
     states = {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN',
-              'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV',
-              'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
-              'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
+              'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT', 'NB', 'NV',
+              'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN',
+              'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
 
     hate_crime_2020_predictions = {}
 
@@ -155,3 +168,43 @@ def find_best_slope(data: List[HateCrime], state: str, start_year: int, end_year
         adjusted_slope = (end_incidents - middle_incidents) // (end_year - middle_year)
 
     return adjusted_slope
+
+
+###############################################################################
+# Calculating percent difference
+###############################################################################
+def instances_in_2020(data: List[HateCrime]) -> dict[str, int]:
+    """Return a dictionary mapping the state to the corresponding number of hate crime instances
+    in 2020."""
+    hate_crime_instances_2020 = {}
+
+    states = {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN',
+              'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT', 'NB', 'NV',
+              'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN',
+              'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
+
+    for state in states:
+        hate_crime_instances_2020[state] = num_instances_by_year(data, state, 2020)
+
+    return hate_crime_instances_2020
+
+
+def calculate_percent_difference(data: List[HateCrime]) -> dict[str, float]:
+    """Return a dictionary mapping the abbreviated state name with the corresponding percent
+    difference between the real and predicted hate crime instances in 2020.
+
+    """
+    percent_difference = {}
+
+    predicted = predictions(data)
+    actual = instances_in_2020(data)
+
+    states = {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN',
+              'IA', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MS', 'MO', 'MT', 'NB', 'NV',
+              'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN',
+              'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WV', 'WI', 'WY'}
+
+    for state in states:
+        percent_difference[state] = (predicted[state] - actual[state]) / actual[state] * 100
+
+    return percent_difference
