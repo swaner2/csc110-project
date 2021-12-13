034c8000638c44789ddfa6afe57146908ff7baab
Jessica <86748529+swaner2@users.noreply.github.com>
2021-12-13 14:18:51 -0500

    Add files via upload

--- covid_to_hate_crime_relationship.py
+++ covid_to_hate_crime_relationship.py
@@ -3,9 +3,8 @@
 
 Module Description
 ==================
-This module contains functions to transform the data about covid cases and hate crime rates in the United States
-into graphs.
-
+This module contains functions to transform the data about covid cases and hate crime rates in the
+United States into graphs.
 """
 import plotly.graph_objects as go
 from hate_crime import HateCrime, num_instances_by_month
@@ -14,14 +13,14 @@ from covid_dataclass import CovidData, processing_data
 
 def get_xy_data(covid_data: list[CovidData], hate_crime_data: list[HateCrime], state: 'str') -> \
         tuple[list[str], list[int], list[int]]:
-    """Return a tuple of 3 parallel lists. The first list contains strings in the format 'month, year'.
+    """Return a tuple of 3 parallel lists.
+    The first list contains strings in the format 'month, year'.
     The second list contains the corresponding number of new covid cases.
     The third list contains the corresponding number of hate crimes.
-
     Preconditions:
-    - len(covid_data) > 0
-    - len(covid_data) = len(hate_crime_data)
-    - state in {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', \
+        - len(covid_data) > 0
+        - len(covid_data) = len(hate_crime_data)
+        - state in {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', \
                          'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', \
                          'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', \
                          'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', \
@@ -36,14 +35,15 @@ def get_xy_data(covid_data: list[CovidData], hate_crime_data: list[HateCrime], s
             date = (x.date.month, 2020)
             list.append(dates, date)
             list.append(covid_nums, x.cases)
-            list.append(hate_crimes, num_instances_by_month(hate_crime_data, state, 2020, x.date.month))
+            list.append(hate_crimes, num_instances_by_month(hate_crime_data, state, 2020,
+                                                            x.date.month))
     return dates, covid_nums, hate_crimes
 
 
-def plot_covid_and_hate_crime(covid_data: list[CovidData], hate_crime_data: list[HateCrime], state: 'str') -> None:
+def plot_covid_and_hate_crime(covid_data: list[CovidData], hate_crime_data: list[HateCrime],
+                              state: 'str') -> None:
     """Plot the relationship between covid rates and hate crime rates for a given state.
     Each point represents a different month in 2020.
-
     Preconditions:
         - state in {'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', \
                          'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', \
@@ -54,12 +54,26 @@ def plot_covid_and_hate_crime(covid_data: list[CovidData], hate_crime_data: list
 
     _, x_data, y_data = get_xy_data(covid_data, hate_crime_data, state)
 
-
-    fig = go.Figure()
     fig = go.Figure(data=go.Scatter(x=x_data, y=y_data, mode='markers'))
-    
+
     fig.update_layout(title=f'The Correlation Between Covid Rates and Hate Crime in {state}',
                       xaxis_title='Covid Rate',
-                      yaxis_title=f'Hate Crime Rate')
-    
+                      yaxis_title='Hate Crime Rate')
+
     fig.show()
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'extra-imports': ['python_ta.contracts', 'plotly.graph_objects', 'hate_crime',
+                          'covid_dataclass'],
+        'disable': ['R1705']
+    })
+
+    import python_ta.contracts
+
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
